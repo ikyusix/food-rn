fbd87a305139bb6b6bc1325ab8811e8af9088ba4
ikyusix <ikyusixadam@gmail.com>
2021-04-26 12:25:01 +0700

    architecture approach

---
+++ src/components/SearchBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {View, Text, StyleSheet} from "react-native";
+
+const SearchBar = () => {
+    return <View>
+        <Text>Search screen</Text>
+    </View>
+}
+
+const styles = StyleSheet.create({})
+
+export default SearchBar
